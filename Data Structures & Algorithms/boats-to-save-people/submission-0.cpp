9e31b970c8c1dc7a29c2c72dafd2035dce0ec0f2
Keerti Vardhan Singh <aryan3800chauhan@gmail.com>
2026-06-30 15:50:12 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& nums, int limit) {
+        sort(nums.begin(),nums.end());
+        
+        int n = nums.size();
+        int i=0;
+        int j=n-1;
+        int count =0;
+
+        while(i<=j){
+            if(nums[i]+ nums[j]<= limit){
+                i++;
+
+            }
+            j--;
+            count++;
+
+        }
+
+        return count ;
+
+
+        
+    }
+};
